08065b19ee8b62c072854ae1feaee16382c61e8b
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2022-10-16 11:57:24 +0200

    Equipo Service añadido

--- src/test/java/madstodolist/EquipoServiceTest.java
+++ src/test/java/madstodolist/EquipoServiceTest.java
@@ -30,3 +30,60 @@ public class EquipoServiceTest {
         assertThat(equipos.get(1).getNombre()).isEqualTo("Proyecto Níquel");
     }
 }
+
+@SpringBootTest
+@Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
+public class EquipoServiceTest {
+
+    @Autowired
+    EquipoService equipoService;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    class TresIds {
+        Long equipo1Id;
+        Long equipo2Id;
+        Long usuarioId;
+        TresIds(Long equipo1Id, Long equipo2Id, Long usuarioId) {
+            this.equipo1Id = equipo1Id;
+            this.equipo2Id = equipo2Id;
+            this.usuarioId = usuarioId;
+        }
+    }
+
+    // Añade dos equipos a la base de datos y un usuario en el primer equipo.
+    // Devuelve el identificador de los equipos y de los usuarios.
+    public TresIds addEquiposBD() {
+        Equipo equipo1 = equipoService.crearEquipo("Proyecto Cobalto");
+        Equipo equipo2 = equipoService.crearEquipo("Proyecto Níquel");
+        Usuario usuario = new Usuario("user@ua");
+        usuario.setPassword("123");
+        usuario = usuarioService.registrar(usuario);
+        equipoService.addUsuarioEquipo(usuario.getId(), equipo1.getId());
+        return new TresIds(equipo1.getId(), equipo2.getId(), usuario.getId());
+    }
+
+
+}
+
+
+    @Test
+    public void obtenerEquipo() {
+        // GIVEN
+        // Un equipo en la base de datos
+        Long equipoId = addEquiposBD().equipo1Id;
+
+        // WHEN
+        Equipo equipoBD = equipoService.findById(equipoId);
+
+        // THEN
+        assertThat(equipoBD.getNombre()).isEqualTo("Proyecto Cobalto");
+        // Comprobamos que la relación con Usuarios es lazy: al
+        // intentar acceder a la colección de usuarios se debe lanzar una
+        // excepción
+        assertThatThrownBy(() -> {
+            equipoBD.getUsuarios().size();
+        }).isInstanceOf(LazyInitializationException.class);
+    }
+    
